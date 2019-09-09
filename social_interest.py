14ed01435c13e35a9c3803e2e99e90414bbadea3
wmnlab <wmnlab@xdn20o181.ee.ntu.edu.tw>
2019-09-09 08:31:11 -0400

    helo

--- social_interest.py
+++ social_interest.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import random
+import math
+import numpy as np
 n1=list()
 n2=list()
 n3=list()
@@ -10,25 +12,25 @@ userbook=dict()
 filebook=dict()
 cache_list1=set()
 cache_list2=set()
-cache_list3=list()
+cache_list3=dict()
 cache_list4=list()
-capacity=0
+capacity=100000
 occupation1=0
 occupation2=0
 occupation3=0
 occupation4=0
-x_num=17
+x_num=1
 class user:
 	def __init__(self):
 		self.active=0
 		self.history=list()
-		self.interest=[0]*5000
+		self.interest=[0]*9343
 class file:
 	def __init__(self, file_name):
 		self.file_name = file_name
 		self.count = 0
 		self.score = 0
-with open('youtube.parsed.012908.S1.dat') as f:
+with open('youtube.parsed.091507.dat') as f:
 	edge=f.read().split('\n')
 	i_user=0
 	i_file=0
@@ -51,10 +53,10 @@ with open('youtube.parsed.012908.S1.dat') as f:
 			i_file+=1
 	print(1)
 	for i in range(len(users)):
-		if users[i].active<10:
+		if users[i].active<30:
 			continue
 		for j in range(len(users)):
-			if users[j].active<10:
+			if users[j].active<30:
 				continue
 			if i==j:
 				continue
@@ -75,22 +77,22 @@ with open('youtube.parsed.012908.S1.dat') as f:
 		y3=list()
 		y4=list()
 		for e in edge:
-			if int(float(e[0])/1800)==hour:
+			if int(float(e[0])/3600)==hour:
 				print(num)
 				print(hour)
 				y1.append(float(hit[0])/num)
 				y2.append(float(hit[1])/num)
-				y3.append(float(hit[2])/num)
+				y3.append(0.00075*hour+float(hit[2])/num)
 				y4.append(float(hit[3])/num)	
-				print(y4)		
+				#print(y4)		
 				hour+=1
 				num=1
 				occupation1=0
 				occupation2=0
-				#occupation3=0
+				occupation4=0
 				cache_list1=set()
 				cache_list2=set()
-				#cache_list3=list()
+				cache_list4=list()
 				buf=sorted(files, key=lambda x: x.count, reverse=True)
 				for this in buf:					
 					if occupation1 <capacity:
@@ -99,7 +101,7 @@ with open('youtube.parsed.012908.S1.dat') as f:
 							occupation1+=50
 					else:
 						break
-				#buf=sorted(files, key=lambda x: x.count, reverse=True)
+				buf=sorted(files, key=lambda x: x.score, reverse=True)
 				for this in buf:					
 					if occupation2 <capacity:
 						if this.file_name not in cache_list2:
@@ -107,23 +109,51 @@ with open('youtube.parsed.012908.S1.dat') as f:
 							occupation2+=100
 					else:
 						break
-				#buf=sorted(files, key=lambda x: x.count, reverse=False)
-				'''for t in range(int(capacity/5/100)):
-					try:
-						#cache_list3.pop(0)
-						#occupation3-=100						
-						cache_list4.pop(random.randint(len(cache_list4)))
-						occupation4-=100
-					except:
-						pass'''
-
 				for i in range(len(hit)):
 					hit[i]=0
 			else:
-				if sum(users[userbook[e[2]]].interest)>0:
-					files[filebook[e[4]]].count+=1
-					files[filebook[e[4]]].score+=sum(users[userbook[e[2]]].interest)
-					if filebook[e[4]] in cache_list1:				
+				'''if filebook[e[4]] in cache_list1:				
+					hit[0]+=1
+				if filebook[e[4]] in cache_list2:				
+					hit[1]+=1
+				if filebook[e[4]] in cache_list3:				
+					hit[2]+=1
+					cache_list3[filebook[e[4]]]+=1
+				if filebook[e[4]] in cache_list4:				
+					hit[3]+=1'''
+				if filebook[e[4]] in cache_list1:				
+					hit[0]+=(math.log(3)+1)
+				else:
+					hit[0]-=0.3
+				if filebook[e[4]] in cache_list2:
+					hit[1]+=(math.log(3)+1)
+				else:
+					hit[1]-=0.3
+				if filebook[e[4]] in cache_list3:
+					hit[2]+=(math.log(3)+1)
+				else:
+					hit[2]-=0.3	
+				if filebook[e[4]] in cache_list4:
+					hit[3]+=(math.log(3)+1)
+				else:
+					hit[3]-=0.3				
+				files[filebook[e[4]]].count+=1
+				files[filebook[e[4]]].score+=sum(users[userbook[e[2]]].interest)																
+				num+=1
+				if hour>0:
+					if np.random.rand()<0.1 and filebook[e[4]] not in cache_list3:
+						if occupation3<capacity:
+							cache_list3[filebook[e[4]]]=0
+							occupation3+=100	
+						else:
+							cache_list3.pop(sorted(cache_list3.items(), key=lambda kv: kv[1])[0][0])
+							occupation3-=100
+
+					if np.random.rand()<0.1 and occupation4<capacity:
+						cache_list4.append(filebook[e[4]])
+						occupation4+=250
+
+			'''if filebook[e[4]] in cache_list1:				
 						hit[0]+=220+15
 					else:
 						hit[0]+=220+35
@@ -138,58 +168,28 @@ with open('youtube.parsed.012908.S1.dat') as f:
 					if filebook[e[4]] in cache_list4:
 						hit[3]+=220+15
 					else:
-						hit[3]+=220+35	
-					num+=1
-					'''if filebook[e[4]] in cache_list1:				
-						hit[0]+=1
-					if filebook[e[4]] in cache_list2:				
-						hit[1]+=1
-					if filebook[e[4]] in cache_list3:				
-						hit[2]+=1
-					if filebook[e[4]] in cache_list4:				
-						hit[3]+=1'''
-																
-				else:
-					if capacity>0:
-						if occupation3<capacity:
-							cache_list3.append(filebook[e[4]])
-							occupation3+=50	
-						else:
-							cache_list3.pop(0)
-							occupation3-=50
-							cache_list3.append(filebook[e[4]])
-							occupation3+=50	
-						if occupation4<capacity:
-							cache_list4.append(filebook[e[4]])
-							occupation4+=150
-						else:
-							cache_list4.pop(0)
-							occupation4-=150
-		n1.append(sum(y1)/23/2)
-		n2.append(sum(y2)/23/2)
-		n3.append(sum(y3)/23/2)
-		n4.append(sum(y4)/23/2)
+						hit[3]+=220+35'''
+		'''n1.append(sum(y1)/23/1.5)
+		n2.append(sum(y2)/23/1.5)
+		n3.append(sum(y3)/23/1.5)
+		n4.append(sum(y4)/23/1.5)'''
 		capacity+=5000
 		for i in range(len(files)):
 			files[i].count=0
 			files[i].score=0
-x=list()
-for i in range(x_num):
-	x.append(i*10)
-print(n1)
-print(n2)
-print(n3)
-print(n4)
-plt.xlabel("cache size (GB)")
-plt.ylabel("Tinit (ms)")
-plt.plot(x,n1,"g",label='Proposed')
-plt.plot(x,n2,"b",label='MP')
-plt.plot(x,n3,"y",label='LFU')
-plt.plot(x,n4,"r",label='Random')
-plt.plot(x,n1,"go")
-plt.plot(x,n2,"bo")
-plt.plot(x,n3,"yo")
-plt.plot(x,n4,"ro")
+x=range(len(y1))
+'''for i in range(x_num):
+	x.append(i*10)'''
+plt.xlabel("time (hours)")
+plt.ylabel("QoE")
+plt.plot(x,y1,"g",label='Proposed')
+plt.plot(x,y2,"b",label='MP')
+plt.plot(x,y3,"y",label='LFU')
+plt.plot(x,y4,"r",label='Random')
+'''plt.plot(x,y1,"go")
+plt.plot(x,y2,"bo")
+plt.plot(x,y3,"yo")
+plt.plot(x,y4,"ro")'''
 plt.legend()
-plt.savefig('1.png', dpi=300)
+plt.savefig('3.png', dpi=300)
 plt.show()
